82351c87458a41864f869ff8e05e745547dddac9
Scott Weinstein <sweinstein27@gmail.com>
2018-11-28 17:57:53 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,33 @@
-class SessionsController < ApplicationController
-  skip_before_action :authenticate_user, only: :create
+class GithubService
 
-  def create
-    response = Faraday.post "https://github.com/login/oauth/access_token", {client_id: ENV["GITHUB_CLIENT"], client_secret: ENV["GITHUB_SECRET"],code: params[:code]}, {'Accept' => 'application/json'}
-    access_hash = JSON.parse(response.body)
-    session[:token] = access_hash["access_token"]
+  attr_reader :access_token
 
-    user_response = Faraday.get "https://api.github.com/user", {}, {'Authorization' => "token #{session[:token]}", 'Accept' => 'application/json'}
-    user_json = JSON.parse(user_response.body)
-    session[:username] = user_json["login"]
-
-    redirect_to '/'
+  def initialize(access_hash = nil)
+    @access_token = access_hash["access_token"] if access_hash
   end
+
+  def authenticate!(client_id, client_secret, code)
+    response = Faraday.post "https://github.com/login/oauth/access_token",
+        {client_id: client_id, client_secret: client_secret, code: code},
+        {'Accept' => 'application/json'}
+    access_hash = JSON.parse(response.body)
+    @access_token = access_hash["access_token"]
+  end
+
+  def get_username
+    user_response = Faraday.get "https://api.github.com/user", {}, {'Authorization' => "token #{self.access_token}", 'Accept' => 'application/json'}
+    user_json = JSON.parse(user_response.body)
+    user_json["login"]
+  end
+
+  def get_repos
+    response = Faraday.get "https://api.github.com/user/repos", {}, {'Authorization' => "token #{self.access_token}", 'Accept' => 'application/json'}
+    repos_array = JSON.parse(response.body)
+    repos_array.map{|repo| GithubRepo.new(repo) }
+  end
+
+  def create_repo(name)
+    Faraday.post "https://api.github.com/user/repos", {name: name}.to_json, {'Authorization' => "token #{self.access_token}", 'Accept' => 'application/json'}
+  end
+
 end
